ce0f666e67c31cdf030aed8a86452a5396b50ec2
Syed Zaidi <smarifz@gmail.com>
2013-03-07 20:29:29 -0800

    MP3 streaming media player.
    Streams MP3 via a webserver.

---
+++ src/com/humazadar/media/myAdapter.java
@@ -0,0 +1,15 @@
+package com.humazadar.media;
+
+import java.util.List;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class myAdapter extends ArrayAdapter{
+
+	public myAdapter(Context context, int textViewResourceId, List objects) {
+		super(context, textViewResourceId, objects);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
